b69ffde25a0632ca871d33228fe38065ab15f319
KeZheng Xiong <CleverCoolBear@outlook.com>
2020-07-22 17:48:42 +0800

    Initial commit

---
+++ avl-tree/main.cpp
@@ -0,0 +1,19 @@
+// avl-tree.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+#include "avl_tree.h"
+
+struct test_s
+{
+    int a, b, c;
+};
+
+AVLTree<test_s> tree;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    return 0;
+}
